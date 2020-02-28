eea431293e14f35c92061aaeb17ef5fa83f5b849
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-02-28 16:38:02 +0800

    completed new file and installed  api for the index page

--- Gemfile
+++ Gemfile
@@ -33,7 +33,6 @@ gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
-
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
 
+require 'open-uri'
+require 'json'
 
-Cocktail.create(name: "Whiskey Sour")
-Cocktail.create(name: "Martini")
-Cocktail.create(name: "Manhattan")
+("a".."b").each do |letter|
+url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
+puts "getting #{letter}"
+response = open(url).read
+cocktail_repo = JSON.parse(response)
+cocktails = cocktail_repo["drinks"]
+next if cocktails.nil?
+cocktails.each do |cocktail|
+  new_cocktail = Cocktail.create(name: cocktail['strDrink'], description: cocktail['strInstructions'])
+  i = 1
+  loop do
+    ingredient_name = cocktail["strIngredient#{i}"]
+    ingredient_description = cocktail["strMeasure#{i}"]
+    break if ingredient_name == nil
+    new_ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+    Dose.create(cocktail: new_cocktail, ingredient: new_ingredient, description: ingredient_description)
+    i += 1
+    end
+  end
+end
